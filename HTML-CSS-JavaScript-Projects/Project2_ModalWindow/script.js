f4794c43072e550f440f97a8ee6ba0cd6d29c898
SahilMankarTech <140509393+SahilMankarTech@users.noreply.github.com>
2023-07-25 21:41:41 +0530

    Add files via upload

---
+++ HTML-CSS-JavaScript-Projects/Project2_ModalWindow/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+//   btnOpenModal[i].addEventListener('click', function () {
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
